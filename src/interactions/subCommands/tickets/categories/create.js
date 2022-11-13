ffb6d6ffee87bc6d6ddbfdcb5d2a5f47a9fd3375
Juan Gómez <juangomez.profesional@gmail.com>
2022-11-13 19:04:16 +0000

    Refactor comando de crear ticket

--- src/interactions/subCommands/tickets/categories/create.js
+++ src/interactions/subCommands/tickets/categories/create.js
@@ -4,14 +4,14 @@ const {INSUFFICIENT_PERMISSIONS} = require("../../../../config/lang.json");
 
 module.exports = {
     async run(bot, interaction) {
-        if (memberIsAdmin(interaction.member)) {
-            let name = interaction.options.getString('name');
-            let description = interaction.options.getString('description');
-            let role = interaction.options.getRole('role');
-            let transcriptionChannel = interaction.options.getChannel("transcription_channel") ?? false;
-            await interaction.reply({content: await createTicketCategory(interaction.guild, name, description, role, transcriptionChannel), ephemeral: true})
-        } else {
-            await interaction.reply({content: INSUFFICIENT_PERMISSIONS, ephemeral: true})
+        if (!memberIsAdmin(interaction.member)) {
+            await interaction.reply({content: INSUFFICIENT_PERMISSIONS, ephemeral: true});
+            return;
         }
+        let name = interaction.options.getString('name');
+        let description = interaction.options.getString('description');
+        let role = interaction.options.getRole('role');
+        let transcriptionChannel = interaction.options.getChannel("transcription_channel") ?? false;
+        await interaction.reply({content: await createTicketCategory(interaction.guild, name, description, role, transcriptionChannel), ephemeral: true})
     }
 }
